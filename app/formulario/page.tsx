bcf59b406ec02e4ea74a7d41eceaae011ecf65ba
cauahenriquereis <caua.henrique.srs@gmail.com>
2026-06-19 17:35:04 -0300

    feat: add name and age inputs to formulario with useState

--- app/formulario/page.tsx
+++ app/formulario/page.tsx
@@ -1,7 +1,18 @@
+"use client";
+
+import { useState } from "react";
+
 export default function Formulario() {
-  return (
+    const [nomeCompleto, setNomeCompleto] = useState("");
+    return (
     <main>
-      <h1>Formulário do Paciente</h1>
+        <h1>Formulário do Paciente</h1>
+        <input
+        type="text"
+        value={nomeCompleto}
+        onChange={(e) => setNomeCompleto(e.target.value)}
+        placeholder="Nome completo"
+        />
     </main>
-  );
+    );
 }
